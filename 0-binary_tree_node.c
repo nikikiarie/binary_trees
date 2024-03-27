97a7a1f16586a249a76c48912b1c8d67d2822b19
nikikiarie <nix.kiariebiz@gmail.com>
2024-03-27 13:22:44 -0700

    README

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -10,10 +10,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_node *new;
 
-	new = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	new = malloc(sizeof(binary_tree_t));
 
 	if (new == NULL)
+	{
 		return (NULL);
+	}
 	new->parent = parent;
 	new->n = value;
 	new->left = NULL;
