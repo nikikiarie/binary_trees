e0a6a429105745d1fd55781520ee9cc15e32a498
nikikiarie <nix.kiariebiz@gmail.com>
2024-03-29 14:56:40 -0700

    retry

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,7 +8,7 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_node *new;
+	binary_tree_t *new;
 
 	new = malloc(sizeof(binary_tree_t));
 
--- binary_trees.h
+++ binary_trees.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <limits.h>
 #include <string.h>
 
 /**
@@ -28,7 +28,7 @@ typedef struct binary_tree_s bst_t;
 typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
-void binary_tree_print(const binary_tree_t *tree)
+void binary_tree_print(const binary_tree_t *tree);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
@@ -50,4 +50,11 @@ int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+
+/**
+static int print_t(const binary_tree_t *tree, int offset, int depth, char **s);
+void binary_tree_print(const binary_tree_t *tree);
+static size_t _height(const binary_tree_t *tree);
+*/
+
 #endif
